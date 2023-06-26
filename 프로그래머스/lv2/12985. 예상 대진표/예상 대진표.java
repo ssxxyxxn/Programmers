a6ebb8ecd50cdd9f75c966e2a744050165507afe
한서연 <70804172+ssxxyxxn@users.noreply.github.com>
2023-06-26 12:31:07 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+
+        while(true){
+
+            a = (a+1)/2;
+            b = (b+1)/2;
+
+            if(a == b)
+                break;
+
+            answer++;
+
+        }
+
+        return answer;
+    }
+}
